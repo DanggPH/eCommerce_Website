d09ae737f2a78e8d2b563be56325f0a7368d70b1
dangg <dangphan2k@gmail.com>
2022-08-18 17:23:48 +0700

    testdayvalues code

--- testcode.js
+++ testcode.js
@@ -1,1 +1,16 @@
+var date = new Date("22/01/2000");
 
+function converttimetoformat(time) {
+    if (time != null) {
+      var date = new Date(time);
+      var datevalues =
+        ("0" + date.getDate()).slice(-2) +
+        "/" +
+        ("0" + (date.getMonth() + 1)).slice(-2) +
+        "/" +
+        date.getFullYear();
+      return datevalues;
+    }
+  }
+var time1 = converttimetoformat(date);
+console.log(time1)
